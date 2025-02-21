081e3b22bafd49225e8bb3905aac4c58e48468cd
6Weapons <89061083+lfhbrown@users.noreply.github.com>
2025-02-20 22:25:41 -0800

    Updated CoinItem componenet to styled component

--- src/components/CoinItem.js
+++ src/components/CoinItem.js
@@ -1,18 +1,45 @@
 import React from "react";
+import styled from "styled-components";
+
+const CoinRow = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  background-color: #26272b;
+  box-shadow: 0px 0px 12px #18191b;
+  border-radius: 8px;
+  margin: 2rem 1rem;
+  padding: 0.7rem 1rem;
+
+  &:hover {
+    transform: scale(1.04);
+    transition: 0.3s ease-in-out;
+    cursor: pointer;
+  }
+`;
+const HideMobile = styled.p`
+  @media screen and (max-width: 720px) {
+    display: none;
+  }
+`;
+const ImageSymbol = styled.div`
+  display: flex;
+  align-items: center;
+`;
 
 const CoinItem = (props) => {
   return (
-    <div className="coin-row">
+    <CoinRow>
       <p>{props.coins.market_cap_rank}</p>
-      <div className="img-symbol">
+      <ImageSymbol>
         <img src={props.coins.image} alt="" />
         <p>{props.coins.symbol}</p>
-      </div>
+      </ImageSymbol>
       <p>{props.coins.current_price}</p>
       <p>{props.coins.price_change_percentage_24h}</p>
-      <p className="hide-mobile">{props.coins.total_volume}</p> {/*hide-mobile in Coins.css */}
-      <p className="hide-mobile">{props.coins.market_cap}</p>
-    </div>
+      <HideMobile>{props.coins.total_volume}</HideMobile>
+      <HideMobile>{props.coins.market_cap}</HideMobile>
+    </CoinRow>
   );
 };
 
--- src/components/Coins.js
+++ src/components/Coins.js
@@ -1,24 +1,51 @@
 import React from "react";
+import styled from "styled-components";
 import CoinItem from "./CoinItem";
 import "./Coins.css";
 
+const Container = styled.div`
+  max-width: 1140px;
+  margin: auto;
+  /* display: flex; */
+`;
+
+const Heading = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  background-color: #26272b;
+  box-shadow: 0px 0px 12px #18191b;
+  border-radius: 8px;
+  margin: 2rem 1rem;
+  padding: 0.7rem 1rem;
+  font-weight: 700;
+`;
+
+const HideMobile = styled.p`
+  @media screen and (max-width: 720px) {
+    .hide-mobile {
+      display: none;
+    }
+  }
+`;
+
 const Coins = (props) => {
   return (
-    <div className="container">
+    <Container>
       <div>
-        <div className="heading">
+        <Heading>
           <p>#</p>
           <p className="coin-name">Coin</p>
           <p>Price</p>
           <p>24h</p>
-          <p className="hide-mobile">Volume</p>
-          <p className="hide-mobile">Mkt Cap</p>
-        </div>
+          <HideMobile>Volume</HideMobile>
+          <HideMobile>Mkt Cap</HideMobile>
+        </Heading>
         {props.coins.map((coins) => {
           return <CoinItem coins={coins} key={coins.id} />;
         })}
       </div>
-    </div>
+    </Container>
   );
 };
 
